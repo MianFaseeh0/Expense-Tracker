48ec4a7df2a896589d804412c80f1c060e1188ef
Mian Faseeh <mianfaseeh057@gmail.com>
2026-09-17 00:02:53 +0500

    added google_ml_kit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() async {
   try {
     if (Firebase.apps.isEmpty) {
       await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
+        options: DefaultFirebaseOptions.currentPatform,
       );
     }
   } on FirebaseException catch (e) {
