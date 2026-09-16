74f9f86182e4c3b2993f156800a51dda644e969b
Mian Faseeh <mianfaseeh057@gmail.com>
2026-09-17 01:18:08 +0500

    init

--- lib 2/data/repositories/image_storage_service.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-
-/// Abstraction over where/how a picked receipt image is persisted so it
-/// survives after the temp file the image picker handed us is gone.
-abstract interface class ImageStorageService {
-  Future<File> persist(File sourceImage);
-}
